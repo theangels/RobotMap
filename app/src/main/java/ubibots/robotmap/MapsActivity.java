36d859ada3a4dc7ade6b6b83956f0b92daea2751
郑楠 <zn373578008@outlook.com>
2015-12-05 00:38:12 +0800

    Update

--- app/src/main/java/ubibots/robotmap/MapsActivity.java
+++ app/src/main/java/ubibots/robotmap/MapsActivity.java
@@ -5,8 +5,8 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -19,7 +19,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
@@ -38,9 +38,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Initial and move the camera
+        LatLng initial = new LatLng(0, 0);
+        mMap.addMarker(new MarkerOptions()
+                .position(initial)
+                .title("Marker in Initial"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(initial));
     }
 }
